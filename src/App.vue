2ae4666ca87d249c8cb7a0ed48ff217487f8bbce
Andrew Wilkinson <andywilkinson1993@gmail.com>
2018-11-13 14:21:55 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app dark>
-  <Navbar rootTab='Dogebot' :navLinks='links'></Navbar>
-  <v-content>
-        <v-container fluid>
-            <router-view></router-view>
-        </v-container>
+    <Navbar rootTab='Dogebot' :navLinks='links'></Navbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-content>
     <v-footer></v-footer>
-</v-app>
+  </v-app>
 </template>
 <script>
 import Navbar from '@/components/Navbar.vue';
