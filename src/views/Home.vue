eea693d995adb62dd3d166db9e37b7daa75162e5
Andrew Wilkinson <andywilkinson1993@gmail.com>
2018-11-13 09:35:29 +0000

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
   <v-container py-5>
-    <v-layout wrap mb-5>
-      <v-flex xs8>
-        <h2 class="title mb-3" id="about-me" v-text="'About Me'" />
-        <p v-text="'Profile chit chat'" />
-      </v-flex>
-      <v-flex xs4> <img src="../assets/logo.svg" :alt="'Blah blah blah'" width="100%" /> </v-flex>
-    </v-layout>
+    <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Profile</h3>
+            <div>London based Software Engineer specialising in the latest technologies within the .NET and JavaScript ecosystem. Strong knowledge and experience in the following languages, frameworks & packages include C#, MVC, ASP.NET, .NET, SQL, JavaScript, Vue.js, Node.js, HTML, CSS, Webpack, REST Web API. Further experience in developing automation frameworks from scratch for large web projects using Selenium & Specflow in C#. Comfortable working in multi disciplinary, agile environments using both front and back end technologies with a core drive and passion for innovation on each project worked on in addition to being self-sufficient and proactive about learning. Other interests include Music Production, Socialising & Blockchain Tech / Cryptocurrency Markets.</div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-chip outline color="red">
+            <v-icon left>build</v-icon>vuejs
+          </v-chip>
+          <v-chip outline color="red">
+            <v-icon left>build</v-icon>language-html5
+          </v-chip>
+          <v-chip outline color="red">
+            <v-icon left>build</v-icon>language-javascript
+          </v-chip>
+            <v-chip outline color="red">
+            <v-icon left>build</v-icon>language-csharp
+          </v-chip>
+          </v-chip>
+            <v-chip outline color="red">
+            <v-icon left>build</v-icon>language-css3
+          </v-chip>
+          </v-chip>
+            <v-chip outline color="red">
+            <v-icon left>build</v-icon>nodejs
+          </v-chip>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 
 
 
