1c2c562c49b6ac21970d6586f56c05f67b4d6710
Andrew Wilkinson <andywilkinson1993@gmail.com>
2018-11-15 13:46:07 +0000

    Update Treeview.vue

--- src/components/Treeview.vue
+++ src/components/Treeview.vue
@@ -1,1 +1,38 @@
-
+<template> 
+ <v-treeview
+      v-model="tree"
+      :open="open"
+      :items="items"
+      activatable
+      item-key="name"
+      open-on-click>
+      
+      <template slot="prepend" slot-scope="{ item, open, leaf }">
+        <v-icon v-if="!item.icon">
+          {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+        </v-icon>
+        <v-icon v-else>
+          {{ icons[item.icon] }}
+        </v-icon>
+      </template>
+      
+ </v-treeview>
+</template>      
+<script>
+export default {
+  props: {
+    tree,
+    open,
+    items,
+    icons
+  },
+  components: {
+  
+  },
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
