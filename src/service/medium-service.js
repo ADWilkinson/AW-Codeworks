c9bbf98ec85583ff4df40f6e191cecb4c8f1b8ec
Andrew Wilkinson <andywilkinson1993@gmail.com>
2018-11-15 11:09:46 +0000

    Update medium-service.js

--- src/service/medium-service.js
+++ src/service/medium-service.js
@@ -10,9 +10,45 @@ export default class MediumService extends BaseService {
       this.api
         .get('https://us-central1-awcodeworks.cloudfunctions.net/mediumData')
         .then(response => {
-          resolve(response);
+          let resData = processMediumData(response);
+          resolve(resData);
         })
         .catch(error => reject(error));
     });
+  }
+  
+  const processMediumData = (data) => {
+   
+  let res = JSON.parse(data);
+  let userRefs = resObj.payload.references;
+  let user = Object.values(userRefs.User);
+  let posts = Object.values(userRefs.Post);
+  let social = Object.values(userRefs.SocialStats);
+  
+  let userProfile = {
+    bio: user.bio,
+    name: user.name,
+    followers: social.userFollowedByCount
+  };
+  
+  let userPosts = [];
+  
+  for(values of convertedPosts)
+  {
+    userPosts.push({
+      title: values.title,
+      subtitle: values.content.subtitle,
+      readingTime: values.virtuals.readingTime,
+      wordCount: values.virtuals.wordCount
+    }); 
+  } 
+  
+  let responseObj = {
+    user: userProfile,
+    posts: userPosts
+  };
+  
+  return responseObj;
+ 
   }
 }
