e6e3c344936d1d20b7aa40b71a919b71221850b3
Andrew Wilkinson <andywilkinson1993@gmail.com>
2018-11-16 12:53:51 +0000

    blog work

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,29 +1,42 @@
 <template>
 <v-container py-5>
-    <!-- <v-layout>
-    <h1>Blog</h1>
-    <p>{{this.userData.name}} // {{this.userData.bio}} // {{this.userData.followers}}</p>
-   
--->
-      
-<template>
+  <v-layout>
+  <v-flex xs12 sm6 offset-sm3>
+    <v-container fluid grid-list-md text-xs-left>
+    <v-layout >
+    <v-flex xs4>
+   <v-avatar
+          :tile="false"
+          :size="70"
+          color="grey"
+        >
+          <img :src="require('../../dist/static/Wordmark_White.svg')" alt="avatar">
+        </v-avatar>
+    </v-flex>
+    <v-flex xs8>
+    <h3>{{this.userData.name}}</h3>
+    <p>{{this.userData.bio}} {{this.userData.followers}}</p>
+    </v-flex>
+          </v-layout>
+  </v-container>
+  </v-flex>
+  </v-layout>
   <v-layout>
     <v-flex xs12 sm6 offset-sm3>
-
-    <v-container v-for="(value, key) in postData" :key="key">
-      <v-card>
+  <v-container v-for="(value, key) in postData" :key="key">
+      <v-card :raised="false" :hover="true" :href="value.url" target="_blank">
         <v-img :contain="true" class="pa-4 mx-4"
           :src="require('../../dist/static/Wordmark_White.svg')"
         ></v-img>
 
-        <v-card-title primary-title>
+        <v-card-title primary-title border class="border-top">
           <div>
-            <h3 class="headline mb-0">{{value.title}}</h3>
-            <div>{{value.subtitle}}</div>
+            <h3 class="headline mb-0 title-colour">{{value.title}}</h3>
+            <p class="pt-2 pb-0">{{value.subtitle}}</p>
           </div>
         </v-card-title>
 
-        <v-card-actions>
+        <v-card-actions class="border-top">
           
           <h5 class="pr-1" flat color="orange">{{Math.round(value.readingTime)}} Minute Read |</h5>
           <h5 flat color="orange">{{value.wordCount}} Words |</h5>
@@ -35,9 +48,6 @@
 
     </v-flex>
   </v-layout>
-</template>
-
- </v-layout>
  </v-container>
 
 </template>
@@ -49,8 +59,7 @@ export default {
       postData: {}
     };
   },
-  computed: {
-  },
+  computed: {},
   created() {
     if (!sessionStorage.getItem('mediumUserData') || !sessionStorage.getItem('mediumPostData')) {
       return this.$store.dispatch('blog/retrieveMediumData').then(() => {
@@ -67,3 +76,14 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.title-colour {
+  color: #a3bad6;
+}
+
+.border-top {
+  border-top: solid thin white;
+}
+</style>
+
